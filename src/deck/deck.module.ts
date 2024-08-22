f26b85c9f9bc9d627eb1ca2bb5149832a5ab2858
Gustavo Ulian Zanardi <134564393+GustavoZanardi15@users.noreply.github.com>
2024-08-22 10:07:38 -0300

    Delete src directory

--- src/deck/deck.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DeckService } from './deck.service';
-import { DeckController } from './deck.controller';
-
-@Module({
-  providers: [DeckService],
-  controllers: [DeckController]
-})
-export class DeckModule {}
